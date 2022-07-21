2eaebbd592d065f4d69113c5ed34c287b3e0e5f4
giuseppemaresca <marescag146@gmail.com>
2022-07-21 17:04:29 +0200

    Prima Parte

---
+++ src/main/java/jana60/model/Pizza.java
@@ -0,0 +1,50 @@
+package jana60.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+@Entity
+@Table(name="pizza")
+
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String descrizione;
+	private Double prezzo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public Double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(Double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/jana60/repository/PizzaRepository.java
@@ -0,0 +1,11 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jana60.model.Pizza;
+
+@Repository
+public interface PizzaRepository extends CrudRepository<Pizza, Integer>{
+
+}
